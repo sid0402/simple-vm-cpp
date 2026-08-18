61890cdb7aabe7dac95f6aad94777f9514bf1b43
sid0402 <sagarwal437@gatech.edu>
2026-08-17 18:29:06 -0700

    added instructions - AND, NOT

--- pipelined/instructions.cpp
+++ pipelined/instructions.cpp
@@ -64,6 +64,39 @@ decoded_instruction decode_instruction(uint16_t instruction) {
             break;
         }
 
+        case OP_AND: {
+            const bool uses_immediate = (instruction >> 5) & 0x1;
+
+            decoded.src1 = (instruction >> 6) & 0x7;
+            decoded.dest = (instruction >> 9) & 0x7;
+            decoded.uses_src1 = true;
+            decoded.uses_src2 = !uses_immediate;
+
+            if (uses_immediate) {
+                decoded.immediate = sign_extend(instruction & 0x1F, 5);
+                decoded.control.immediate_format = IMMEDIATE_FORMAT::IMM5;
+                decoded.control.alu_input = ALU_INPUT::IMM;
+            } else {
+                decoded.src2 = instruction & 0x7;
+                decoded.control.alu_input = ALU_INPUT::REG;
+            }
+
+            decoded.control.alu_op = ALU_OPERATION::AND;
+            decoded.control.REG_WRITE = true;
+            decoded.control.CC_WRITE = true;
+            break;
+        }
+
+        case OP_NOT:
+            decoded.src1 = (instruction >> 6) & 0x7;
+            decoded.dest = (instruction >> 9) & 0x7;
+            decoded.uses_src1 = true;
+            decoded.uses_src2 = false;
+            decoded.control.alu_op = ALU_OPERATION::NOT;
+            decoded.control.REG_WRITE = true;
+            decoded.control.CC_WRITE = true;
+            break;
+
         default:
             break;
     }
